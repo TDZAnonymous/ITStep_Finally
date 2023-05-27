8cc387d8304b5a22a791fc39778d68dd22c95377
TDZAnonymous <132370945+TDZAnonymous@users.noreply.github.com>
2023-05-27 18:31:16 +0400

    Add files via upload

---
+++ OtherPages/main.js
@@ -0,0 +1,18 @@
+var $menuToggle = $('#menu-toggle');
+var $menu = $('.menu');
+
+
+
+$menuToggle.click(function() {
+  $menu.toggle();
+});
+
+
+
+$(document).click(function(event) {
+  var $target = $(event.target);
+
+  if (!$target.closest('nav').length) {
+    $menu.hide();
+  }
+});
